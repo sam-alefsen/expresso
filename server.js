8f3c948014d81c12773217d8a3c91effe9f7d954
ghost-lee-ocean <ghostleeocean@gmail.com>
2021-03-12 12:05:49 -0500

    set up server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const cors = require('cors')
+  , errorHandler = require('error-handler')
+  , express = require('express')
+  , morgan = require('morgan');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.static('.'));
+
+//body parsing
+app.use(express.json());
+
+//cors
+app.use(cors());
+
+//morgan
+app.use(morgan('dev'));
+
+//error handling (only necessary for dev environment)
+app.use(errorHandler());
+
+app.listen(PORT, () => {
+  console.log(`Server is now listening on PORT ${PORT}`);
+});
