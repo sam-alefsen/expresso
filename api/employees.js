7a4dc7b88a62467a21638df7b0879d197f50bbbe
ghost-lee-ocean <ghostleeocean@gmail.com>
2021-03-12 14:43:51 -0500

    create employees GET request

--- api/employees.js
+++ api/employees.js
@@ -5,4 +5,15 @@ const employeesRouter = express.Router();
 
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+//GET all employees
+employeesRouter.get('/', (req, res, next) => {
+  db.all(`SELECT * FROM Employee WHERE is_current_employee = 1`, (err, rows) => {
+    if(err) {
+      next(err);
+    } else {
+      res.status(200).json({employees:rows});
+    };
+  });
+});
+
 module.exports = employeesRouter;
