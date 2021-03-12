4aa375a757f83cf347430ea6887c54b152195d01
ghost-lee-ocean <ghostleeocean@gmail.com>
2021-03-12 17:46:02 -0500

    add employees POST request

--- api/employees.js
+++ api/employees.js
@@ -16,4 +16,32 @@ employeesRouter.get('/', (req, res, next) => {
   });
 });
 
+//POST all employees
+employeesRouter.post('/', (req, res, next) => {
+  const name = req.body.employee.name,
+    position = req.body.employee.position,
+    wage = req.body.employee.wage,
+    isCurrentEmployee = req.body.employee.isCurrentEmployee === 0 ? 0 : 1;
+  if(!name || !position || !wage) {
+    return res.sendStatus(400);
+  };
+
+  //backtick notation returns deprecation warning [DEP0097]
+  db.run('INSERT INTO Employee (name, position, wage, is_current_employee) VALUES ($name, $position, $wage, $isCurrentEmployee)', {
+    $name: name,
+    $position: position,
+    $wage: wage,
+    $isCurrentEmployee: isCurrentEmployee
+  }, function(err) {
+    if (err) {
+      next(err);
+    } else {
+      db.get(`SELECT * FROM Employee WHERE Employee.id = ${this.lastID}`,
+        (err, employee) => {
+          res.status(201).json({employee: employee});
+        });
+    };
+  });
+});
+
 module.exports = employeesRouter;
