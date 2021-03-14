831c4ffa65e0bacac35ff0fbcbe8fd09607c651f
ghost-lee-ocean <ghostleeocean@gmail.com>
2021-03-14 18:35:38 -0400

    add menu ID check

--- api/menus.js
+++ api/menus.js
@@ -4,6 +4,20 @@ const { NamedModulesPlugin } = require('webpack');
 const menusRouter = express.Router();
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+//Check menu ID parameter
+menusRouter.param('menuId', (req, res, next, menuId) => {
+  db.get('SELECT * FROM Menu WHERE id = $menuId', (err, menu) => {
+    if(err) {
+      next(err);
+    } else if(menu) {
+      req.menu = menu;
+      next();
+    } else {
+      res.sendStatus(404);
+    };
+  });
+});
+
 //GET all menus
 menusRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM Menu', (err, rows) => {
